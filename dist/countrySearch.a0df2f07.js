18df0a8cf563f369a2e1239c97dae4fc8843e2b5
Elena Gramlich <elenagramlich@Elenas-MacBook-Pro.local>
2022-02-15 22:13:13 +0100

    bijna klaar

--- dist/countrySearch.a0df2f07.js
+++ dist/countrySearch.a0df2f07.js
@@ -468,7 +468,7 @@ var _axiosDefault = parcelHelpers.interopDefault(_axios);
 // 3.  Je plaatst dit dan in een try catch constructie
 // 4.  Het request zelf (endpoint, type)
 //          -  dit is bijvoorbeeld de GET naar
-//          -  Voor vlag, naam en currency kijk je naar all GET https://restcountries.com/v2/name/peru//
+//          -  Voor vlag, naam en currency kijk je naar all GET https://restcountries.com/v2/name/
 //          -  je maakt hier een functie voor
 // 5.  Je checkt het dan met een console.log
 // 6.  Je zet in je HTML een form
@@ -476,26 +476,31 @@ var _axiosDefault = parcelHelpers.interopDefault(_axios);
 // 7.   keyup event listener die moet je halen uit target.value
 //          -   Log en check dit dan vervolgens zodat je weet wat erin staat
 // 8.   Je maakt dan een event listener die luistert naar een enter of klik (pagina moet niet refreshen = default)
-// 9.   Maak hier een if else statement die je koppeld aan je API
 // 10. Probeer het op de pagina te krijgen (index html container maken en die naar javascript halen)
 // 11.  Ga nu bezig met stylen
 async function getCountryInfo(name) {
     const containerResult = document.getElementById("results");
+    const errorMessage = document.getElementById('error-message');
+    containerResult.innerHTML = ` `;
+    errorMessage.innerHTML = ` `;
     try {
         const result = await _axiosDefault.default.get(`https://restcountries.com/v2/name/${name}`);
         console.log(result.data);
         const countries = result.data[0];
         containerResult.innerHTML = `
-        <img src="${countries.flag}" width="200px">
+        <img src="${countries.flag}" alt="country-flag" width="200px">
         <h3> ${countries.name}</h3>
         <p>${countries.name} is situated in ${countries.subregion} and has a population of ${countries.population}</p>
         <p>The capital is ${countries.capital} ${getCurrencies(countries.currencies)} </p>
+       
         `;
     } catch (e) {
         console.error(e);
+        errorMessage.innerHTML = `
+        <p>${name} doesn't exist. Try again</p>
+        `;
     }
 }
-getCountryInfo();
 function getCurrencies(currencies) {
     if (currencies.length === 2) return `and you can pay with ${currencies[0].name} and ${currencies[1].name}`;
     else return `you can pay with ${currencies[0].name}`;
